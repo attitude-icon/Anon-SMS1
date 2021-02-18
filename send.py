4629f6b095f3d84baa30b60b7c5217cc5565f412
Attitude Icon <62551380+attitude-icon@users.noreply.github.com>
2021-02-18 15:56:52 +0530

    Update send.py

--- send.py
+++ send.py
@@ -76,8 +76,7 @@ def update():
 clr()
 banner()
 print('\tChecking For Updates...')
-ver = urllib.request.urlopen(
-    "https://raw.githubusercontent.com/HACK3RY2J/Anon-SMS/master/.version").read().decode('utf-8')
+ver = urllib.request.urlopen("https://raw.githubusercontent.com/HACK3RY2J/Anon-SMS/master/.version").read().decode('utf-8')
 verl = ''
 try:
     verl = open(".version", 'r').read()
@@ -91,8 +90,7 @@ print("Congratulation")
 print("Your Version is Up-To-Date")
 print('\n\tStarting Anon-SMS...\n\n')
 try:
-    noti = urllib.request.urlopen(
-        "https://raw.githubusercontent.com/HACK3RY2J/Anon-SMS/master/.notify").read().decode('utf-8')
+    noti = urllib.request.urlopen("https://raw.githubusercontent.com/HACK3RY2J/Anon-SMS/master/.notify").read().decode('utf-8')
     if len(noti) > 10:
         print('\n\tNotification : ' + noti + '\n\n')
 except Exception:
